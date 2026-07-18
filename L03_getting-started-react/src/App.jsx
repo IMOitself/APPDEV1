ac314bba9b28e96adbc60b0968dd6ccb833f52f7
IMO <180146096+IMOitself@users.noreply.github.com>
2026-07-18 14:53:51 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Russell Jonn T. Bautista</h1>
+      <h2>BSIS3</h2>
+      <p>
+        I usually watch hollywood and korean tv shows and movies.<br/> 
+        Sometimes, if I'm motivated enough, I code in my free time.<br/>
+        I spend my day as a student planning expenses and doing my hobbies.<br/> 
+        Of course, I do activities and reviewing for exams but that's common sense.<br/>
+        I have learned <strong>Java</strong> 2 years before its get taught in class here in college.<br/>
+        Although I only got to learn HTML, CSS, JS, PHP, SQL and Python here in La Verdad.<br/>
+        I also have made several android java projects, though most of them are unfinished. :D
+      </p>
+      <hr/>
+      <p>
+        Based on the lesson so far, I have learned so many things.<br/>
+        I now learn how to use <code>git revert</code> and <code>git rebase</code><br/>
+        I also only just learned what and how to use SSH 
+      </p>
+    </>
+  )
+}
+
+export default App
